f0cca2395e3965ea9c7980a924ca7d5b41309d97
John Eckert <johnteckert@gmail.com>
2020-01-18 12:23:56 -0500

    added basic variables and functions

--- HPTracker.ino
+++ HPTracker.ino
@@ -18,20 +18,21 @@ void loop() {
   int deathSuccess = 0;
   int deathFailure = 0;
 
-  int currentSpellSlotsOne = 4;
-  int maxSpellSlotsOne = 4;
-  int currentSpellSlotsTwo = 3;
-  int maxSpellSlotsTwo = 3;
-  int currentSpellSlitsThree = 2;
-  int maxSpellSlitsThree = 2;
+  int currentLvl1= 4;
+  int maxLvl1= 4;
+  int currentLvl2 = 3;
+  int maxLvl2 = 3;
+  int currentLvl3 = 2;
+  int maxLvl3 = 2;
 
-  void increaseHP() {
+// HP funcs
+  void incHP() {
     if (currentHP < maxHP ) {
       currentHP++;  
     }
   }
 
-  void decreaseHP() {
+  void decHP() {
     if (currentHP > 0) {
       currentHP--;
     }
@@ -40,15 +41,141 @@ void loop() {
     }
   }
 
+  void incTempHP()
+  {
+    tempHP++;
+  }
+
+  void decTempHP()
+  {
+    if (tempHP > 0)
+    {
+      tempHP--;
+    }
+  }
+// Inspiration Funcs
+  void incInsp()
+  {
+    if (currentInsp < maxInsp)
+    {
+      currentInsp++;
+    }
+  }
+
+  void decInsp()
+  {
+    if (currentInsp > 0)
+    {
+      currentInsp--;
+    }
+  }
+// Death Save Funcs
+  void incDeathSuccess()
+  {
+    if (deathSuccess <= 3) {
+      deathSuccess++;
+    }
+  }
+
+  void decDeathSuccess()
+  {
+    if (deathSuccess > 0)
+    {
+      deathSuccess--;
+    }
+  }
+
+  void incDeathFailure()
+  {
+    if (deathFailure <= 3)
+    {
+      deathFailure++;
+    }
+  }
+
+  void decDeathFailure()
+  {
+    if (deathFailure > 0)
+    {
+      deathFailure--;
+    }
+  }
+
   void deathSaves() {}
 
+// Spell Funcs
+  void incLvl1()
+  {
+    if (currentLvl1 < maxLvl1)
+    {
+      currentLvl1++;
+    }
+  }
+
+  void decLvl1()
+  {
+    if (currentLvl1 > 0)
+    {
+      currentLvl1--;
+    }
+    else
+    {
+      deathSaves();
+    }
+  }
+
+  void incLvl2()
+  {
+    if (currentLvl2 < maxLvl2)
+    {
+      currentLvl2++;
+    }
+  }
+
+  void decLvl2()
+  {
+    if (currentLvl2 > 0)
+    {
+      currentLvl2--;
+    }
+    else
+    {
+      deathSaves();
+    }
+  }
+
+  void incLvl3()
+  {
+    if (currentLvl3 < maxLvl3)
+    {
+      currentLvl3++;
+    }
+  }
+
+  void decLvl3()
+  {
+    if (currentLvl3 > 0)
+    {
+      currentLvl3--;
+    }
+    else
+    {
+      deathSaves();
+    }
+  }
+
+  // Long Rest
   void longRest() {
    currentHP = maxHP;
    tempHP = 0;
    currentInsp = maxInsp;
+
    deathSuccess = 0;
    deathFailure = 0;
-   
+
+   currentLvl1 = maxLvl1;
+   currentLvl2 = maxLvl2;
+   currentLvl3 = maxLvl3;
   }
   
 }
